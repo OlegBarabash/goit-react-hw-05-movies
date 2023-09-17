fe7d150a3a968ec67bcf0f4cc11c9b1c33255caa
OlegBarabash <olegbarabash7@gmail.com>
2023-09-18 00:06:01 +0300

    Test fix1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,8 +24,8 @@ export const App = () => {
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-          <Route path="*" element={<Navigate to="/" />} />
         </Route>
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
   );
